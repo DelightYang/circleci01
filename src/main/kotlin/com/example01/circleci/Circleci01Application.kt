7d9b0281482206c1d5e3f94d10952b2cfd06db92
Yang <cutesunshine@icloud.com>
2019-09-08 18:23:03 +0800

    update repo

---
+++ src/main/kotlin/com/example01/circleci/Circleci01Application.kt
@@ -0,0 +1,11 @@
+package com.example01.circleci
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Circleci01Application
+
+fun main(args: Array<String>) {
+	runApplication<Circleci01Application>(*args)
+}
---
+++ src/test/kotlin/com/example01/circleci/Circleci01ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example01.circleci
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Circleci01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
